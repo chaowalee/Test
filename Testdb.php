4e47454ef9ae745573e077a9ede801c166c22b22
chaowalee <42596892+chaowalee@users.noreply.github.com>
2018-08-22 11:16:52 +0700

    Add files via upload

--- Testdb.php
+++ Testdb.php
@@ -5,3 +5,5 @@ $query_office = mysqli_query($conn,$sql_office);
 while ($obj = mysqli_fetch_array($query_office))
 {
  echo $obj["office_name"]."<br>"; //จุด คือการเชื่อมข้อความ , "<br>" คือการขึ้นบรรทัดใหม่
+}
+?>
