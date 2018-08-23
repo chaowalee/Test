148030e6fe83a9e46eccad64c5c0a2f75bff7307
chaowalee <42596892+chaowalee@users.noreply.github.com>
2018-08-23 16:25:39 +0700

    Add files via upload

---
+++ db/connect-db-PQ.php
@@ -0,0 +1,11 @@
+<?php
+	$server = "us-cdbr-iron-east-01.cleardb.net";
+    $username = "b21e535520af4b";
+    $password = "402bbf1f";
+    $db = "heroku_821969a41e3a17e";
+    $conn = new mysqli($server, $username, $password, $db);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    mysqli_query($conn, "SET NAMES utf8");
